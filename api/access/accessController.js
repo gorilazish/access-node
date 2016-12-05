229e0765ec983d2f4ce7474019b0937e83d617cb
Ignas <ignas.bagdon@gmail.com>
2016-12-05 16:24:25 +0100

    refactor granting access for cards

--- api/access/accessController.js
+++ api/access/accessController.js
@@ -6,54 +6,54 @@ exports.post = function (req, res, next) {
     var lock_access_lvl = parseInt(req.body.access_lvl);
 
     /** If card_id is sent in the body it means user is using physical card */
-    if(req.body.card_id !== undefined) {
+    if (req.body.card_id !== undefined) {
         var card_id = req.body.card_id;
 
-        // Checking in institution DB if there exists card with this id and access_lvl
-        return Card.findOne({ card_id: card_id, access_lvl: lock_access_lvl }, function (err, card) {
-            if(err) {
-                 console.error(err)
-            } else if(!card) {
-                 res.json({
-                    success: false,
-                    message: 'No card found.'
-                })
-            } else if(card) {
-                 res.json({
-                    success: true,
-                    message: 'Access granted for the card.'
-                })
-            }
-        })
+        Card.findOne({card_id: card_id, access_lvl: lock_access_lvl})
+            .then(function (card) {
+                if (!card) {
+                    res.json({
+                        success: false,
+                        message: 'No card found.'
+                    })
+                } else if (card) {
+                    res.json({
+                        success: true,
+                        message: 'Access granted for the card.'
+                    })
+                }
+            }, function (err) {
+                next(err)
+            });
     }
 
     /** If we get here, user is using his phone to get access */
     var UID = req.body.user_id;
 
     // Checking in OUR DB if there is a user with this UID, and has a relevant card with access_lvl
-    return User.findOne({ user_id: UID }, function (err, user) {
-        if(err) {
-            console.error(err);
-        }
-        if(!user){
-             res.json({
-                success: false,
-                message: 'No user found.'
-            })
-        } else if(user) {
-            user.cards.map(function (card) {
-                if(card.institution == institution && card.access_lvl !== lock_access_lvl) {
-                    res.json({
-                        success: false,
-                        message: 'Invalid access level.'
-                    })
-                } else if(card.institution == institution && card.access_lvl == lock_access_lvl){
-                    res.json({
-                        success: true,
-                        message: 'Access granted for the phone.'
-                    })
-                }
-            })
-        }
-    })
+    User.findOne({user_id: UID, 'cards.institution': institution})
+        .then(function (user) {
+            if(!user) {
+                res.json({
+                    success: false,
+                    message: 'No card found.'
+                })
+            }
+            return user.cards.map(function (card) {
+                    if (card.access_lvl !== lock_access_lvl) {
+                        return res.json({
+                            success: false,
+                            message: 'Invalid access level.'
+                        })
+                    } else if (card.access_lvl == lock_access_lvl) {
+                        return res.json({
+                            success: true,
+                            message: 'Access granted for the phone.'
+                        })
+                    }
+                })
+        }, function (err) {
+            next(err)
+        })
 };
+
